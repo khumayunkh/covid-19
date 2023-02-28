96929fc87c3935e02ea3a60f9cb363e83a52af22
khumayunkh <hasanovhumayun15@gmail.com>
2023-02-28 16:21:00 +0500

    header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,13 +1,14 @@
 import React from "react";
 import style from './Header.module.sass'
 import logo from './../../assets/logo.png'
+import mask from './../../assets/Reusable-Mask.png'
 
 function Header(){
     return(
         <>
         <div className={style.container}>
             <div className={style.menu}>
-                <img/>
+                <img src={logo}/>
                 <div className={style.items}>
                     <a>Home</a>
                     <a>About</a>
@@ -19,21 +20,26 @@ function Header(){
                 <button className={style.contact_btn}>Contacts</button>
             </div>
             <div className={style.content}>
-                <h4>Made in Britain</h4>
-                <h1>
-                    A game changer in
-                    the ethical fight against
-                    Covid-19
-                </h1>
-                <p>
-                    Textiles are one of the main vectors for 
-                    viruses and bacteria. Textiles treat with HeiQ 
-                    viroblock help stop cross- 
-                    contamination from surface to face covering.
-                </p>
-                <div>
-                    <button>Shop Now</button>
-                    <button>Learn More</button>
+                <div className={style.content__header}>
+                    <h4>Made in Britain</h4>
+                    <h1>
+                        A game changer in
+                        the ethical fight against
+                        Covid-19
+                    </h1>
+                    <p>
+                        Textiles are one of the main vectors for 
+                        viruses and bacteria. Textiles treat with HeiQ 
+                        viroblock help stop cross- 
+                        contamination from surface to face covering.
+                    </p>
+                    <div className={style.content__button}>
+                        <button className={style.content__button_1}>Shop Now</button>
+                        <button className={style.content__button_2}>Learn More</button>
+                    </div>
+                </div>
+                <div className={style.content__img}>
+                    
                 </div>
             </div>
         </div>
