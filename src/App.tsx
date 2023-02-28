d045b0b44a4c9741f3584bfeee8034797d142dcf
khumayunkh <hasanovhumayun15@gmail.com>
2023-02-28 15:17:47 +0500

    header page

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,10 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
 import './App.sass'
+import Header from './components/Header/Header'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <div className="App">
-      
+      <Header/>
     </div>
   )
 }
---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,44 @@
+import React from "react";
+import style from './Header.module.sass'
+import logo from './../../assets/logo.png'
+
+function Header(){
+    return(
+        <>
+        <div className={style.container}>
+            <div className={style.menu}>
+                <img/>
+                <div className={style.items}>
+                    <a>Home</a>
+                    <a>About</a>
+                    <a>The Secience</a>
+                    <a>Shop Bulk</a>
+                    <a>Ordering</a>
+                    <a>News</a>
+                </div>
+                <button className={style.contact_btn}>Contacts</button>
+            </div>
+            <div className={style.content}>
+                <h4>Made in Britain</h4>
+                <h1>
+                    A game changer in
+                    the ethical fight against
+                    Covid-19
+                </h1>
+                <p>
+                    Textiles are one of the main vectors for 
+                    viruses and bacteria. Textiles treat with HeiQ 
+                    viroblock help stop cross- 
+                    contamination from surface to face covering.
+                </p>
+                <div>
+                    <button>Shop Now</button>
+                    <button>Learn More</button>
+                </div>
+            </div>
+        </div>
+        </>
+    )
+}
+
+export default Header
